5305b74540cdd1064cb14b5515c7bb947eff98c7
XiangQian2000 <xiangqian8711@gmail.com>
2026-09-24 14:38:31 +0300

    Complete phonebook exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Filter = ({ filter, onFilterChange }) => {
   return (
@@ -35,7 +35,7 @@ const Persons = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <p key={person.name}>
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
@@ -44,16 +44,18 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then((response) => response.json())
+      .then((data) => {
+        setPersons(data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
